bd096093e9127bab63c5805135c16d409b791078
Eileen <eniedenfuehr@gmail.com>
2024-09-12 14:23:52 +0200

    remove more episodes

--- refine_tuna.py
+++ refine_tuna.py
@@ -3,8 +3,8 @@
 # Date: 2024-09-06 15:30:04
 
 # As location plays no role in referencegame, all episodes with +LOC condition
-# are removed. Descriptions have location in them so they dont serve my purpose.
-# Currently removes files from furniture folder in sg condition only.
+# are removed. Descriptions have location in them so they dont serve my.
+# purpose. Currently removes files from furniture folder in sg condition only.
 
 # Parse all episodes in corpus folder und remove
 # those with +LOC condition
@@ -26,11 +26,14 @@ def rm_loc(directory):
                     if data["TRIAL"]["@CONDITION"] == "+LOC":
                         os.remove(file_path)
                         print(f"Removed file: {filename}")
+                    elif "META-ATTRIBUTE" in data["TRIAL"]["DESCRIPTION"]:
+                        os.remove(file_path)
+                        print(f"Removed file: {filename}")
 
                 except json.JSONDecodeError:
 
                     print(f"Error reading {filename}, skipping file.")
-    print("All files with +LOC condition removed.")
+    print("All files with +LOC condition and META-ATTRIBUTE removed.")
 
 
 if __name__ == "__main__":
