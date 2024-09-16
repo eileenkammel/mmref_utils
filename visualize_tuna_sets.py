d9bf4932d1aa1ca3489bfcb0db803e47fec2b95b
Eileen <eniedenfuehr@gmail.com>
2024-09-16 10:46:49 +0200

    visualize of7 episodes

--- visualize_tuna_sets.py
+++ visualize_tuna_sets.py
@@ -53,6 +53,35 @@ def extract_relevant_info(input_dir, output_name):
 
     return output_name
 
+
+# Make csv file from only the original 7 trials
+def extract_og_trials(input_file, output_file):
+    csv_header = ["TRIAL_ID", "T", "D1", "D2", "D3", "D4", "D5", "D6"]
+    csv_data = []
+
+    with open(input_file, "r") as file:
+        data = json.load(file)
+    for trial in data["TRIALS"]:
+        trial_id = trial["@ID"]
+        target_image = None
+        distractor_images = []
+
+        for entity in trial["ENTITIES"]:
+            if entity["@TYPE"] == "target":
+                target_image = entity["@IMAGE"]
+            elif entity["@TYPE"] == "distractor":
+                distractor_images.append(entity["@IMAGE"])
+
+        csv_data.append(
+            [trial_id] + [target_image] + distractor_images[:6]
+        )
+
+    with open(output_file, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(csv_header)
+        writer.writerows(csv_data)
+    return output_file
+
 # Sort the CSV file by the target image name
 
 
@@ -101,12 +130,25 @@ def create_html_images(text_html, image_path, output_name):
 
 
 if __name__ == "__main__":
-    directory = "tuna"
-    output_csv_name = "tuna.csv"
-    output_html_name = "tuna.html"
-    output_html_images = "tuna_images.html"
+    # For all trials
+    # directory = "tuna"
+    # output_csv_name = "tuna.csv"
+    # output_html_name = "tuna.html"
+    # output_html_images = "tuna_images.html"
+    # tuna_images = "tuna_original/dist/images/furniture"
+    # tuna_csv = extract_relevant_info(directory, output_csv_name)
+    # tuna_csv = sort_csv(tuna_csv)
+    # tuna_html = create_html(tuna_csv, output_html_name)
+    # create_html_images(tuna_html, tuna_images, output_html_images)
+
+    # For the original 7 trials
+    og7_file = "og7.json"
+    og7_csv = "og7.csv"
+    og7_html = "og7.html"
+    og7_html_images = "og7_images.html"
     tuna_images = "tuna_original/dist/images/furniture"
-    tuna_csv = extract_relevant_info(directory, output_csv_name)
-    tuna_csv = sort_csv(tuna_csv)
-    tuna_html = create_html(tuna_csv, output_html_name)
-    create_html_images(tuna_html, tuna_images, output_html_images)
+    og7_csv = extract_og_trials(og7_file, og7_csv)
+    og7_csv = sort_csv(og7_csv)
+    og7_html = create_html(og7_csv, og7_html)
+    create_html_images(og7_html, tuna_images, og7_html_images)
+
